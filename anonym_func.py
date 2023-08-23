caffbe6d86de1970d44c18286a66f2f629c9ef7c
Azzi-oo <123190729+Azzi-oo@users.noreply.github.com>
2023-08-24 00:29:21 +0500

    Create anonym_func.py

---
+++ anonym_func.py
@@ -0,0 +1,7 @@
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+#is_words = lambda x: len(x) == 6
+#output = ''.join(sorted(filter(is_words, words)))
+#print(output)
+is_words = lambda x: len(x) == 6
+output = ' '.join(sorted(filter(is_words, words)))
+print(output)
